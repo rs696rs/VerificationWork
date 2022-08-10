e4e60a8058394f4910a7fd1704727fb8c9529511
«rs696» <rs696@yandex.ru>
2022-08-10 20:05:56 +0300

    program is written

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+string[] Array3Characters(string[] array)
+{
+    int count = 0;
+    string[] array2 = new string[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+
+    }
+    Array.Resize(ref array2, count);
+    return array2;
+}
+
+string[] arrayWords = { "yes", "ussr", "usa", "to", "1234", "1567", "a", "Stalin", "gulag", "b", "c", "no" };
+string[] newArrayWords = Array3Characters(arrayWords);
+
+Console.Write("[" + string.Join(", ", newArrayWords) + "]");
